22705fe57cafb23841ffb82a99ff6478e559899e
FXDCommunity <166769846+FXDCommunity@users.noreply.github.com>
2024-05-15 08:48:58 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,78 @@
+let questions = [
+    {
+        numb: 1,
+        question:
+            "Game FPS Terkenal Tapi DiTinggalkan DiKarenakan Banyaknya Cheater Adalah?",
+        answer: "A. CSGO",
+        options: [
+            "A. CSGO",
+            "B. Free Fire",
+            "C. Point Blank",
+            "D. Call Of Duty"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Game Minecraft BerKategori Sebagai Game?",
+        answer: "D. Sandbox+Adventure",
+        options: [
+            "A. RPG",
+            "B. Adventure",
+            "C. Sandbox",
+            "D. Sandbox+Adventure"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Game Solo Leveling : ARISE Terinspirasi Dari Anime?",
+        answer: "B. Solo Leveling",
+        options: [
+            "A. One Piece",
+            "B. Solo Leveling",
+            "C. One Punch Man",
+            "D. Tidak Ada"
+        ]
+    },
+    {
+        numb: 4,
+        question: "???",
+        answer: "???",
+        options: ["???", "???", "???", "???"] 
+    },
+    {
+        numb: 5,
+        question: "???",
+        answer: "???",
+        options: ["???", "???", "???", "???"]
+    },
+    {
+        numb: 6,
+        question: "???",
+        answer: "???",
+        options: ["???", "???", "???", "???"]
+    },
+    {
+        numb: 7,
+        question: "???",
+        answer: "???",
+        options: ["???", "???", "???", "???"]
+    },
+    {
+        numb: 8,
+        question: "???",
+        answer: "???",
+        options: ["???", "???", "???", "???"]
+    },
+    {
+        numb: 9,
+        question: "???",
+        answer: "???",
+        options: ["???", "???", "???", "???"]
+    },
+    {
+        numb: 10,
+        question: "???",
+        answer: "???",
+        options: ["???", "???", "???", "???"]
+    }
+];
